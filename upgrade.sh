24a8709af915081af1fc040c488de36b98447023
Dudu Twizer <dudu@twizer.net>
2023-07-18 23:52:19 +0300

    setting repo to dudutwizer

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /root
-curl -o main.zip https://codeload.github.com/mluggy/teslabox/zip/refs/heads/main
+curl -o main.zip https://codeload.github.com/dudutwizer/teslabox/zip/refs/heads/main
 unzip -o main.zip
 cp -r teslabox-main/* teslabox
 rm -rf teslabox-main
